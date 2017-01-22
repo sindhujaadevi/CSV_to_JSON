5afa84937b4562717ef70250cca7df00e0f0d162
sindhujaadevi <pencil4sind@gmail.com>
2017-01-22 15:39:57 +0000

    Index_Finding

--- js/food.js
+++ js/food.js
@@ -7,5 +7,12 @@ readfile.on('data', function(chunk) {
 readfile.on('end', function() {
   let line = data.split('\n');
   let head = line[0].split(',');
-  fs.writeFile('../outputdata/header.json', JSON.stringify(head), 'utf-8');
+
+  let countries = ['Netherlands', 'Canada', 'United Kingdom', 'United States', 'Australia', 'France', 'Germany', 'Spain', 'South Africa'];
+  let countryIndex = head.indexOf('countries_en');
+  let saltIndex = head.indexOf('salt_100g');
+  let sugarIndex = head.indexOf('sugars_100g');
+
+  console.log(countryIndex + '/' + saltIndex + '/' + sugarIndex); //33/116/102
+  //fs.writeFile('../outputdata/header.json', JSON.stringify(head), 'utf-8');
 });
