68dc41a05d0259f5f8ae3aaf0537a93d17c8dd37
sindhujaadevi <pencil4sind@gmail.com>
2017-01-23 12:35:58 +0000

    another_new

---
+++ js/converters/TemperatureConverter.js
@@ -0,0 +1,5 @@
+module.exports = function (e){
+  let c=parseInt(e);
+var v=Math.round(c * 1.8)+32;
+return v;
+}
--- js/food.js
+++ js/food.js
@@ -8,18 +8,28 @@ readfile.on('end', function(){
   let line = data.split('\n');
   let head = line[0].split(',');
   let allContent = [];
-  let saltcontent = new Array(9).fill(0);
-  let sugarcontent = new Array(9).fill(0);
+  let saltcontent =  Array(9).fill(0);
+  let sugarcontent =  Array(9).fill(0);
 
-  let countries = ['Netherlands', 'Canada', 'United Kingdom', 'United States', 'Australia', 'France', 'Germany', 'Spain', 'South Africa'];
   let countryIndex = head.indexOf('countries_en');
   let saltIndex = head.indexOf('salt_100g');
   let sugarIndex = head.indexOf('sugars_100g');
 
+  let countries = ['Netherlands', 'Canada', 'United Kingdom', 'United States', 'Australia', 'France', 'Germany', 'Spain', 'South Africa'];
+  var northEurope = ['United Kingdom', 'Denmark', 'Sweden', 'Norway'];
+  var centralEurope = ['France', 'Belgium', 'Germany', 'Switzerland', 'Netherlands'];
+  var southEurope = ['Portugal', 'Greece', 'Italy', 'Spain', 'Croatia', 'Albania'];
+
+  var allCountry = [];
+  var allC;
+
   for(let i = 1; i<line.length; i++)
   {
+
     let content = line[i].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
     //allContent.push(content);
+    allC = content[countryIndex];
+    allCountry.push(allC);
     let flag1 = countries.includes(content[countryIndex]);
     if(flag1){
       let index = countries.indexOf(content[countryIndex]);
@@ -35,14 +45,8 @@ sugarvalue = 0;
 saltcontent[index] = saltcontent[index] + parseFloat(saltvalue);
 sugarcontent[index] = sugarcontent[index] + parseFloat(sugarvalue);
     }
+
   }
-  for(let j=0; j<countries.length; j++){
-    let obj = {};
-    obj['country'] = countries[j];
-    obj['salt'] = saltcontent[j];
-    obj['sugar'] = sugarcontent[j];
-    allContent.push(obj);
-    console.log(allContent);
-  }
-  fs.writeFile('../outputdata/part1.json',JSON.stringify(allContent), 'utf-8');
+  console.log(JSON.stringify(allCountry);
+//  fs.writeFile('../outputdata/part1.json',JSON.stringify(allContent), 'utf-8');
 });
